cf84c7cab45a45963b4f8f4f1b1f3d2df61697dd
Mozart Al Khateeb <mozart.alkhateeb@gmail.com>
2023-07-25 03:18:10 +0300

    chore: release(5.0.0) feat: recreate demo app with using latest ionic cli

--- _demo/AngularMicrophone/src/app/app.module.ts
+++ _demo/AngularMicrophone/src/app/app.module.ts
@@ -9,9 +9,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
